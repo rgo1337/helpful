1fa08de4a0586761802232368b969e2f1a4c8c42
rgo1337 <131670206+rgo1337@users.noreply.github.com>
2023-09-11 15:34:10 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://github.com/rgo1337/helpful/blob/main/build.func)
 # Copyright (c) 2021-2023 tteck
 # Author: tteck (tteckster)
 # License: MIT
